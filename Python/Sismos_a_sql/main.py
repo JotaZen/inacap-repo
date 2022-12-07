afbb219348c6bac662c32eec63f13048b730f357
JotaZen <106561804+JotaZen@users.noreply.github.com>
2022-12-07 13:10:39 -0300

    Update main.py

--- Python/Sismos_a_sql/main.py
+++ Python/Sismos_a_sql/main.py
@@ -1,17 +1,27 @@
 import requests, json
+from datetime import date
 from classes import ConexionDB
 
+NOMBRE_BASE_DE_DATOS = 'sismos'
 API = 'https://chilealerta.com/api/query/?user=demo&select=ultimos_sismos&country=chile'
 
 def actualizarDBSismos():
     response = requests.get(API)
     if response.status_code == 200:
         data = json.loads(response.text.encode('UTF-8'))
-        db = ConexionDB(database='python')
+        fecha = date.today()
+        db = ConexionDB()
+        try:
+            with db.cursor() as cursor:
+                query = f'CREATE DATABASE {NOMBRE_BASE_DE_DATOS}'
+                cursor.execute(query)
+                db.connection.commit()
+        except: pass
+        db = ConexionDB(database=NOMBRE_BASE_DE_DATOS)
 
         try:
             with db.cursor() as cursor:
-                query = f'DROP TABLE SISMOS'
+                query = f'DROP TABLE `SISMOS({fecha})`;'
                 cursor.execute(query)
                 db.connection.commit()
         except: pass
@@ -25,7 +35,7 @@ def actualizarDBSismos():
                 columns2 += f',`{key}`'
             columns += ')'   
             columns2 = '(' + columns2[1:] +')'   
-            query = f'CREATE TABLE SISMOS {columns}'
+            query = f'CREATE TABLE `SISMOS({fecha})` {columns}'
             cursor.execute(query)
             values = ""
             for i in data['ultimos_sismos_chile']:
@@ -36,10 +46,10 @@ def actualizarDBSismos():
                 values_l = '(' + values_l[1:] + ')'
                 values += values_l + ","
             values = values[:-1]
-            query = f'INSERT INTO SISMOS{columns2} VALUES {values}'
+            query = f'INSERT INTO `SISMOS({fecha})`{columns2} VALUES {values}'
             cursor.execute(query)
             db.connection.commit()
-            print('Se guardaron los últimos registros de sismos')
+            print(f'Se guardaron los últimos registros de sismos del día {fecha}.')
 
 
 if __name__ == '__main__':
